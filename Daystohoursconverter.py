dda45ec74851bdbfcac0abe490b8b187b2b28052
DevArv <106604262+DevArv@users.noreply.github.com>
2022-06-01 11:12:30 -0400

    Update test

--- Daystohoursconverter.py
+++ Daystohoursconverter.py
@@ -1,18 +1,21 @@
-to_units = 24
-name_to_unit = "hours"
 
 
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * to_units} {name_to_unit}"
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days are {num_of_days * 24 * 60} {conversion_unit} minutes"
+    else:
+        return "unsupported unit"
 
 
 def validate_and_execute():
     try:
-        user_input_number = int(num_of_days_element)
+        user_input_number = int(days_and_unit_dictionary["days"])
 
         # we want to do a conversion only for positive integers
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0:
             print("You have entered a 0, please enter a valid positive number")
@@ -24,14 +27,10 @@ def validate_and_execute():
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Hey dear user, enter number of days as a comma separated list and I will conv ert it to hours:\n")
-    list_of_days = user_input.split(", ")
-
-    print(list_of_days)
-    print(set(list_of_days))
-
-    print(type(list_of_days))
-    print(type(set(list_of_days)))
-
-    for num_of_days_element in set(user_input.split(", ")):
-        validate_and_execute()
+    user_input = input("Hey dear user, enter number of days and conversion unit!:\n")
+    days_and_unit = user_input.split(":")
+    print(days_and_unit)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    print(type(days_and_unit_dictionary))
+    validate_and_execute()
