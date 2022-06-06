fa4ca70cbed9eca8207e796564fa7c3d42a23d5c
DevArv <106604262+DevArv@users.noreply.github.com>
2022-06-06 09:03:45 -0400

    Update: Still having an issue

---
+++ mainsocial.py
@@ -0,0 +1,11 @@
+from loguser import User
+from post import Post
+
+app_user_one = User("test@test.com", "Joan Alexander", "psw11", "Data analyst py")
+app_user_one.get_user_info()
+
+app_user_two = User("test1@gmail.com", "agent Demo", "psw22", "Assistant py")
+app_user_two.get_user_info()
+
+new_post = Post("this post is a test", app_user_two.name)
+new_post.get_post_info()
