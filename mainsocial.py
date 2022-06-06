b58ba8c0f5c2ed645c87ac2e8e3d5ac37b7c6f50
DevArv <106604262+DevArv@users.noreply.github.com>
2022-06-06 10:37:17 -0400

    Update: Delete code - modPost.

--- mainsocial.py
+++ mainsocial.py
@@ -1,5 +1,4 @@
 from loguser import User
-from modpost import Post
 
 app_user_one = User("test@test.com", "Joan Alexander", "psw11", "Data analyst py")
 app_user_one.get_user_info()
@@ -7,5 +6,3 @@ app_user_one.get_user_info()
 app_user_two = User("test1@gmail.com", "agent Demo", "psw22", "Assistant py")
 app_user_two.get_user_info()
 
-new_post = Post("this post is a test", app_user_two.name)
-new_post.get_post_inf()
