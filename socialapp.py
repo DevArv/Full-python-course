b6a1d6e168a7e8b6fc3eca874760c16a12d24fdd
DevArv <106604262+DevArv@users.noreply.github.com>
2022-06-06 17:00:41 -0400

    Update: I need RE.code post module. It's not working yet.

--- socialapp.py
+++ socialapp.py
@@ -1,8 +1,6 @@
-from loguser import User
-
+from user import User
 app_user_one = User("test@test.com", "Joan Alexander", "psw11", "Data analyst py")
 app_user_one.get_user_info()
 
 app_user_two = User("test1@gmail.com", "agent Demo", "psw22", "Assistant py")
 app_user_two.get_user_info()
-
