a6b55af8a5bc9e114f30c797f1903c498591c192
DevArv <106604262+DevArv@users.noreply.github.com>
2022-06-06 20:37:12 -0400

    Update: Post module it's done.

--- socialapp.py
+++ socialapp.py
@@ -1,6 +1,14 @@
-from user import User
-app_user_one = User("test@test.com", "Joan Alexander", "psw11", "Data analyst py")
+import user
+import post
+
+app_user_one = user.User("test@test.com", "Joan Alexander", "psw11", "Data analyst py")
 app_user_one.get_user_info()
 
-app_user_two = User("test1@gmail.com", "agent Demo", "psw22", "Assistant py")
+app_user_two = user.User("test1@gmail.com", "agent Demo", "psw22", "Assistant py")
 app_user_two.get_user_info()
+
+new_post = post.Post("Now we can upload the module. Work its done", app_user_two.name)
+new_post.get_post_info()
+
+new_post = post.Post("Almost a Dev Data analyst of py", app_user_one.name)
+new_post.get_post_info()
