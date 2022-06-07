6353a64e69e91f17086dd051ddfd6b05f0b35e0d
DevArv <106604262+DevArv@users.noreply.github.com>
2022-06-07 10:58:45 -0400

    API - GitHub / Not working yet

--- liveprojects.py
+++ liveprojects.py
@@ -1,9 +1,9 @@
 import requests
 
 Username = "DevArv"
-response = requests.get("https://api.github.com/orgs/DevArv/repos")
+response = requests.get("https://api.github.com/users/DevArv")
 my_projects = response.json()
 print(my_projects)
 
 for project in my_projects:
-    print(f"Project name: {project['name']} Project url: {project['repos_url']}")
+    print(f"Project name: {project['name']}, Project url: {project['web_url']}")
